f2892e98ac850b19b164ba17f99571fc18b5d613
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-06-19 20:14:07 +0600

    table row & col CRUD query added

--- intro.sql
+++ intro.sql
@@ -31,3 +31,69 @@ CREATE TABLE users(
     email VARCHAR(266) UNIQUE NOT NULL,
     age int DEFAULT 18
 )
+
+
+INSERT INTO users VALUES (1, 'abc', 'abc@gmail.com');
+
+-- Insert with column name
+INSERT INTO users (userId, userName, email, age) VALUES (2, 'abcd', 'abc@gmail.com', 30)
+
+-- Insert many rows
+INSERT INTO users (username, email) VALUES 
+('nahid', 'nahid@gmail.com'),
+('hossain', 'hossain@gmail.com'),
+('dfa', 'dfa@gmail.com');
+
+-- ## Alter table
+
+-- ADD EXTRA COLUMN with default valuE
+ALTER TABLE users 
+add column password VARCHAR(250) DEFAULT 'a12345' NOT NULL;
+
+-- update all column value
+UPDATE  users
+SET password = '12345'
+
+-- delete a column 
+ALTER TABLE users 
+drop COLUMN age
+
+-- add column with null value
+ALTER TABLE users add COLUMN demo int;
+
+-- change data type
+ALTER TABLE users alter COLUMN demo type TEXT;
+
+-- set default value
+ALTER Table users alter COLUMN demo set DEFAULT 'something';
+
+-- remove default value
+ALTER Table users alter COLUMN demo drop DEFAULT;
+
+
+--  rename column name
+ALTER Table users
+RENAME COLUMN demo to country;
+
+-- add constraint to existing column
+ALTER TABLE users
+    alter COLUMN country set not null;
+
+-- remove constraint to existing column
+ALTER TABLE users
+    alter COLUMN country drop not null;
+
+
+-- Add unique constrain to existing column
+ALTER TABLE users
+ADD constraint unique_email UNIQUE(email);
+
+-- Drop unique constrain to existing column
+ALTER TABLE users
+DROP constraint unique_email;
+
+
+SELECT * from users
+
+
+
