304d6b61fdf34863006d2aafce9a3fd3004ab623
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-06-20 23:20:41 +0600

    practice task done

---
+++ tasks.sql
@@ -0,0 +1,167 @@
+-- Active: 1718885498155@@127.0.0.1@5432@task_1
+
+-- https://github.com/Apollo-Level2-Web-Dev/postgresSQL-practice-task
+
+ALTER DATABASE task_1 RENAME to tasks
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2)
+);
+
+INSERT INTO employees (emp_id, emp_name, department, salary)
+VALUES
+    (1, 'John Doe', 'HR', 50000.00),
+    (2, 'Jane Smith', 'IT', 60000.00),
+    (3, 'Michael Johnson', 'Finance', 55000.00),
+    (4, 'Emily Brown', 'HR', 52000.00);
+
+-- Task: 1
+SELECT emp_name, salary FROM employees WHERE department='HR';
+ 
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (order_id, customer_id, total_amount)
+VALUES
+    (101, 1, 200.00),
+    (102, 2, 300.00),
+    (103, 1, 150.00),
+    (104, 3, 400.00),
+    (105, 2, 250.00);
+
+-- Task 2
+SELECT customer_id, AVG(total_amount) AS average_total_amount 
+FROM orders
+GROUP BY customer_id 
+
+
+
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10)
+);
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50),
+    credits INT
+);
+
+INSERT INTO students (student_id, student_name, age, gender)
+VALUES
+    (1, 'Alice', 22, 'Female'),
+    (2, 'Bob', 21, 'Male'),
+    (3, 'Charlie', 23, 'Male');
+
+INSERT INTO courses (course_id, course_name, credits)
+VALUES
+    (101, 'Mathematics', 3),
+    (102, 'History', 4),
+    (103, 'Physics', 3);
+
+-- Enrollment table with student-course relationships
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT
+);
+
+INSERT INTO enrollment (enrollment_id, student_id, course_id)
+VALUES
+    (1, 1, 101),
+    (2, 1, 102),
+    (3, 2, 103),
+    (4, 3, 101);
+
+-- Task - 3
+SELECT
+ students.student_name,
+ courses.course_name,
+ courses.credits
+ FROM enrollment
+ JOIN courses ON enrollment.course_id = courses.course_id
+ JOIN students ON enrollment.student_id = students.student_id;
+
+--  Task - 4
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department_id INT
+);
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+
+CREATE TABLE salaries (
+    emp_id INT,
+    salary DECIMAL(10, 2)
+);
+
+INSERT INTO employees (emp_id, emp_name, department_id)
+VALUES
+    (1, 'John Doe', 1),
+    (2, 'Jane Smith', 2),
+    (3, 'Michael Johnson', 1),
+    (4, 'Emily Brown', 3);
+
+INSERT INTO departments (department_id, department_name)
+VALUES
+    (1, 'HR'),
+    (2, 'IT'),
+    (3, 'Finance');
+
+INSERT INTO salaries (emp_id, salary)
+VALUES
+    (1, 50000.00),
+    (2, 60000.00),
+    (3, 55000.00),
+    (4, 52000.00);
+
+
+-- Write an SQL query to retrieve the department name and the average salary
+--  of employees working in each department. Sort the results by the average 
+--  salary in descending order.
+SELECT 
+    d.department_name, 
+    ROUND(AVG(s.salary), 2)
+    FROM employees e
+    JOIN departments d ON e.department_id = d.department_id
+    JOIN salaries s ON s.emp_id = e.emp_id
+    GROUP BY d.department_name
+    ORDER BY AVG(s.salary) DESC
+
+
+-- Task - 5
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (order_id, customer_id, order_date, total_amount)
+VALUES
+    (101, 1, '2023-01-05', 200.00),
+    (102, 2, '2023-01-06', 300.00),
+    (103, 1, '2023-02-10', 150.00),
+    (104, 3, '2023-02-15', 400.00),
+    (105, 2, '2023-03-20', 250.00);
+
+SELECT 
+    to_char(order_date, 'Month'), 
+    sum(total_amount), 
+    count(*)
+    FROM orders
+    GROUP BY to_char(order_date, 'Month')
+    ORDER BY count(*) DESC
